de5fbef0e8a99301a1f7a2203eb1eb1396da2507
steven0129 <h3584935@gmail.com>
2018-08-29 20:53:50 +0800

    Analysis of dependency tree

--- main.py
+++ main.py
@@ -43,5 +43,26 @@ def skipgram(**kwargs):
         os.system(f'./fasttext skipgram -input tmp.txt -output {options.sgm_result}/skipgram -dim {options.word_dim} -epoch {options.sgm_epochs} -ws {options.sgm_ws} -lrUpdateRate {options.sgm_lr_update_rate} -thread {options.thread}')
         os.remove('./tmp.txt')
 
+def SEST(**kwargs):
+    for k_, v_ in kwargs.items():
+        setattr(options, k_, v_)
+
+    S = ltp.Sentence()
+    W = ltp.Word(dictDir=DICT_DIR)
+
+    with open(f'{DATA_DIR}/sky_dragon.txt') as f:
+        contents = filter(lambda x: not x.startswith('['), f.readlines())
+        contents = map(lambda x: x.rstrip('\n'), list(contents))
+        content = ''.join(list(contents))
+
+    print('斷句中...')
+    sents = S.split(content)
+
+    print('斷詞中...')
+    wordLists = list(map(W.split, tqdm(sents)))
+    
+    print(wordLists[0])
+    print(S.parse(wordLists[0]))
+
 if __name__ == '__main__':
   fire.Fire()
