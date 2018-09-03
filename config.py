cdc679826bff363c5313281cec536ca4db9e4b9d
steven0129 <h3584935@gmail.com>
2018-09-03 13:07:17 +0800

    Remove preprocess.csv

--- config.py
+++ config.py
@@ -1,8 +1,15 @@
 class Env(object):
-    thread = 8
+    thread = 8  # Number of thread 
     word_dim = 300  # Dimension of word vector
     sgm_epochs = 1000  # Fasttext skipgram epochs
     sgm_result = 'wordvec'  # Direction of result
     sgm_ws = 5  # Size of the context window
     sgm_lr_update_rate = 100  # Change the rate of updates for the learning rate
+    
+    sest_dim = 300
+    sest_epochs = 5000
+    sest_result = 'sentvec'
+    sest_ws = 5
+    sest_lr_update_rate = 100
+    
     cuda = True
